373ab27592d3b01a1f7fbe497762b4a93c5bccf4
macabeus <bruno.macabeus@gmail.com>
2020-01-23 01:09:27 +0000

    chore: add basic codebase for web project

---
+++ web/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <span>Hello Switch from Web!</span>
+)
+
+ReactDOM.render(<App />, document.getElementById('app'))
