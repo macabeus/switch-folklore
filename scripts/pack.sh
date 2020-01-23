dc3cf185784dddc5c5e7d945420eacb69570698c
macabeus <bruno.macabeus@gmail.com>
2020-01-23 01:08:13 +0000

    scripts: add pack

---
+++ scripts/pack.sh
@@ -0,0 +1,4 @@
+mkdir -p sd/atmosphere/contents/0100000000000011
+cp sysmodule/sysmodule.nsp sd/atmosphere/contents/0100000000000011/exefs.nsp
+cp sysmodule/toolbox.json sd/atmosphere/contents/0100000000000011/toolbox.json
+cp sysmodule/config.json sd/atmosphere/contents/0100000000000011/switch-folklore.json
